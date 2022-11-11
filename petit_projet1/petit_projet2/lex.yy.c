110d040d42a2d8c77bfb6d948ff702cd46e0e209
SOME <sourbar2001@gmail.com>
2022-11-11 04:20:24 -0800

    a3

--- petit_projet1/petit_projet2/lex.yy.c
+++ petit_projet1/petit_projet2/lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 2
-#define YY_END_OF_BUFFER 3
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,18 +360,19 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[11] =
+static const flex_int16_t yy_accept[13] =
     {   0,
-        0,    0,    3,    2,    2,    0,    0,    0,    1,    0
+        0,    0,    0,    0,    5,    4,    4,    3,    3,    1,
+        2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    2,    1,    1,    1,    1,    3,    1,    1,    1,
+        1,    3,    1,    1,    1,    1,    4,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,33 +398,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[4] =
+static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    2,    1
+        1,    1,    2,    3
     } ;
 
-static const flex_int16_t yy_base[14] =
+static const flex_int16_t yy_base[16] =
     {   0,
-        8,    7,    9,   12,    0,    6,    5,    3,   12,   12,
-        0,    3,    2
+        8,    7,    0,    0,   10,   13,    0,   13,    0,   13,
+       13,   13,    4,    7,    5
     } ;
 
-static const flex_int16_t yy_def[14] =
+static const flex_int16_t yy_def[16] =
     {   0,
-       11,   11,   10,   10,   12,   13,   13,    7,   10,    0,
-       10,   10,   10
+       13,   13,   12,    3,   12,   12,   14,   12,   15,   12,
+       12,    0,   12,   12,   12
     } ;
 
-static const flex_int16_t yy_nxt[16] =
+static const flex_int16_t yy_nxt[18] =
     {   0,
-        4,    4,    7,    7,    6,    9,    8,    8,   10,    5,
-        5,    3,   10,   10,   10
+        8,    8,    9,    8,    6,    6,    6,   11,   10,   12,
+        7,    7,    5,   12,   12,   12,   12
     } ;
 
-static const flex_int16_t yy_chk[16] =
+static const flex_int16_t yy_chk[18] =
     {   0,
-       11,   11,   13,   13,   12,    8,    7,    6,    3,    2,
-        1,   10,   10,   10,   10
+        3,    3,    3,    3,   13,   13,   13,   15,   14,    5,
+        2,    1,   12,   12,   12,   12,   12
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -440,17 +441,16 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "analyseurC1.flex"
+#line 1 "analyseurC3.flex"
 #define YY_NO_INPUT 1
-#line 3 "analyseurC1.flex"
-#include "proto-color.h"  
-void echo(char *lex_cat) {
-    fprintf(stdout,GREEN("[%s:%s]"), lex_cat, yytext); 
-}
+#line 3 "analyseurC3.flex"
+#include "proto-color.h"
+#line 449 "lex.yy.c"
+
 #line 451 "lex.yy.c"
-#line 452 "lex.yy.c"
 
 #define INITIAL 0
+#define COMMENT 1
 
 #ifndef YY_NO_UNISTD_H
 /* Special case for "unistd.h", since it is non-ANSI. We include it way
@@ -663,7 +663,7 @@ YY_DECL
 		}
 
 	{
-#line 8 "analyseurC1.flex"
+#line 7 "analyseurC3.flex"
 
 #line 669 "lex.yy.c"
 
@@ -692,13 +692,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 11 )
+				if ( yy_current_state >= 13 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 12 );
+		while ( yy_base[yy_current_state] != 13 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -723,18 +723,29 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
-/* rule 1 can match eol */
 YY_RULE_SETUP
-#line 9 "analyseurC1.flex"
-echo("commentaire");
+#line 8 "analyseurC3.flex"
+{ BEGIN(COMMENT); SET_COLOR(1);}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 10 "analyseurC1.flex"
+#line 9 "analyseurC3.flex"
+{ BEGIN(INITIAL); SET_COLOR(0); }
+	YY_BREAK
+case 3:
+/* rule 3 can match eol */
+YY_RULE_SETUP
+#line 10 "analyseurC3.flex"
+{ ECHO; }
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 12 "analyseurC3.flex"
 ECHO;
 	YY_BREAK
-#line 737 "lex.yy.c"
+#line 747 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
+case YY_STATE_EOF(COMMENT):
 	yyterminate();
 
 	case YY_END_OF_BUFFER:
@@ -1030,7 +1041,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 11 )
+			if ( yy_current_state >= 13 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1058,11 +1069,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 11 )
+		if ( yy_current_state >= 13 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 10);
+	yy_is_jam = (yy_current_state == 12);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1701,8 +1712,9 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 10 "analyseurC1.flex"
+#line 12 "analyseurC3.flex"
+
 
 int yywrap(void) { return 1; }
-int main(int argc, char *argv[]) { while (yylex()!=0) ; return 0; } 
+int main(int argc, char *argv[]) { while (yylex()!=0) ; return 0; }
 
